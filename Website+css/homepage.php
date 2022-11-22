95c8cad23f9f1b72cd8bb1e0c8f6fb72d057f797
Hanna Hasson <hsoonhanna0@gmail.com>
2022-11-22 16:01:55 +0100

    fead<web>: uplod content-area in homepage.php

--- Website+css/homepage.php
+++ Website+css/homepage.php
@@ -19,4 +19,49 @@
             <a ><i class="material-icons">account_circle</i></a>
         </div>
     </header>
+    <div class="content-area">
+        <aside>
+            <nav>
+                <a href="" class="nav-link active">
+                    <i class="material-icons">home</i>
+                    <span id="active-span">Home</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">language</i>
+                    <span id="active-span">Language</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">done</i>
+                    <span id="active-span">Done</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">person_add</i>
+                    <span id="active-span">New Account</span>
+                </a>
+                <hr>
+                <a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a>
+                <a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a><a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a><a href="" class="nav-link">
+                    <i class="material-icons">add</i>
+                    <span id="active-span">Add</span>
+                </a>
+            </nav>
+        </aside>
+
     </body>
