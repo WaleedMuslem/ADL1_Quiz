ae763c818b14127d482ad6b102dd529c50367103
Hanna Hasson <hsoonhanna0@gmail.com>
2022-11-22 15:58:25 +0100

    feat<web>:uplod header in homepage.php

---
+++ Website+css/homepage.php
@@ -0,0 +1,22 @@
+<?php require_once "header.php"; ?>
+
+<body>
+    <header>
+        <div class="left_section">
+            <i class="material-icons-outlined long">menu</i>
+            <img src="images/icon.png">
+        </div>
+        <div class="mid_section">
+            <form action="">
+                <input type="text" placeholder="Search"/>
+            <button><i class="material-icons">search</i></button>
+            </form>
+            <i class="material-icons mic">mic</i>
+        </div>
+        <div class="right_section">
+            <a href="login.php"><i class="material-icons">login</i></a>  
+            <a href="register.php"><i class="material-icons">person_add</i></a>
+            <a ><i class="material-icons">account_circle</i></a>
+        </div>
+    </header>
+    </body>
