2afceeb3c18a90d23296a773762074c7c3f00ae7
MaximusDD2002 <2maxim2@mail.ru>
2022-11-26 23:43:34 +0100

    Added a config file
    Added a config file that assists database connection

---
+++ Website+css/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('BASE_URL','.../ADL1_Quiz/Project_Files/');
+define('BASE_PATH',__DIR__);
+
+define('DEBUG', true);
+
+/**
+ * DATABASE SETTINGS
+ */
+
+define('DB_HOST', 'localhost:3306');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'adl1_quiz');
