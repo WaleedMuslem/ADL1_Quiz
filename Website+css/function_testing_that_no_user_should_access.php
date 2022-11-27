7870884655cb0f1f77bb3b079363e7dfabb98f98
MaximusDD2002 <2maxim2@mail.ru>
2022-11-27 20:33:46 +0100

    Added a file for testing
    Added a file that assists function testing. It mustn't be accessible by users

---
+++ Website+css/function_testing_that_no_user_should_access.php
@@ -0,0 +1,26 @@
+<?php
+require_once('functions.php');
+$connection = db_connect();
+$quizes = get_them_quizes($connection);
+$questions = get_questions_by_quiz_id($connection, 1);
+$answers = get_da_answer($connection, 1);
+//$make = create_quiz($connection, "3","3","4",1);
+//$eg = register($connection, "5","5","5","5",1);
+//$log = login($connection, "'Admin@Admin.com'", "'Admin'");
+//$log2 = login($connection, '3', '3');
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+debug_to_console(json_encode($quizes));
+debug_to_console(json_encode($questions));
+debug_to_console(json_encode($answers));
+//debug_to_console(json_encode($make));
+//debug_to_console(json_encode($eg));
+//debug_to_console(json_encode($log));
+//debug_to_console(json_encode($log2));
