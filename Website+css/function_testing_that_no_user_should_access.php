aa427e1368a0b4e6198c4cad8d717ec3044f7c29
MaximusDD2002 <2maxim2@mail.ru>
2022-11-28 01:25:20 +0100

    Imrpoved the file for testing
    Imrpoved the file for testing by changing the login and registration function test according to the changes in the code

--- Website+css/function_testing_that_no_user_should_access.php
+++ Website+css/function_testing_that_no_user_should_access.php
@@ -5,9 +5,9 @@ $quizes = get_them_quizes($connection);
 $questions = get_questions_by_quiz_id($connection, 1);
 $answers = get_da_answer($connection, 1);
 //$make = create_quiz($connection, "3","3","4",1);
-//$eg = register($connection, "5","5","5","5",1);
+//$eg = register($connection, "6","6","6");
 //$log = login($connection, "'Admin@Admin.com'", "'Admin'");
-//$log2 = login($connection, '3', '3');
+$log2 = login($connection, '6', '6');
 
 function debug_to_console($data) {
     $output = $data;
@@ -23,4 +23,4 @@ debug_to_console(json_encode($answers));
 //debug_to_console(json_encode($make));
 //debug_to_console(json_encode($eg));
 //debug_to_console(json_encode($log));
-//debug_to_console(json_encode($log2));
+debug_to_console(json_encode($log2));
