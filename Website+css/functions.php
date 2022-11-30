6582295eba26c8e7e5b675efa2fd53e19e905232
MaximusDD2002 <2maxim2@mail.ru>
2022-11-30 19:30:54 +0100

    Updated the functions
    Changed the names of some functions so it would be easier to understand what they do and made some comments for them that explain how they work

--- Website+css/functions.php
+++ Website+css/functions.php
@@ -7,6 +7,7 @@ function is_post() {
     return $_SERVER['REQUEST_METHOD'] === 'POST';
 }
 
+// Connects to the database
 function db_connect() {
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(mysqli_connect_errno()) {
@@ -32,6 +33,7 @@ function confirm_query($result_set) {
     }
 }
 
+// Looks if the user exists in the database and allows him to log in if he does
 function login($connection, $email, $password){
    /* $username = mysqli_real_escape_string($connection, $_POST['username']);
     $email = mysqli_real_escape_string($connection, $_POST['email']);
@@ -74,6 +76,7 @@ function login($connection, $email, $password){
     return $row;
 }
 
+// Allows the registration of an user by writing his data into the database. Of course, the data has some checks on it
 function register($connection, $username, $email, $password){
 
     if (empty($username)) {
@@ -102,7 +105,8 @@ function register($connection, $username, $email, $password){
     //}
 }
 
-function get_them_quizes($connection){
+// Fetches the list of all quizes with coresponding data
+function get_all_quizes($connection){
     if (empty($connection)) {
         $msg = "Database connection error";
       //} elseif (empty($quiz_columns) || !is_array($quiz_columns)) {
@@ -132,6 +136,7 @@ function get_them_quizes($connection){
       return $msg;
 }
 
+// Creates a quiz and attaches user's id to it. He'll e considered to be a creator
 function create_quiz($connection, $category, $image, $title, $user_id){
 
     if (empty($title)) {
@@ -157,6 +162,9 @@ function create_quiz($connection, $category, $image, $title, $user_id){
     //}
 }
 
+// Edits quizes if the user is permited to.
+//The lvl 0 admin can edit everything,
+//while lvl 1 teacher can edit only those things that were created by him and have his user id attached
 function edit_quiz($connection, $user_id, $quiz_id, $category, $image, $title){
     $user_lvl = mysqli_query($connection, "SELECT level_of_user FROM users WHERE $user_id = user_id limit 1");
     $quiz_owner_id = mysqli_query($connection, "SELECT user_id FROM quizzes WHERE quiz_id = $quiz_id limit 1");
@@ -167,6 +175,9 @@ function edit_quiz($connection, $user_id, $quiz_id, $category, $image, $title){
     } else {return 0;}
 }
 
+// Edits questions if the user is permited to.
+//The lvl 0 admin can edit everything,
+//while lvl 1 teacher can edit only those things that were created by him and have his user id attached
 function edit_questions($connection, $user_id, $quiz_id, $content){
     $user_lvl = mysqli_query($connection, "SELECT level_of_user FROM users WHERE $user_id = user_id limit 1");
     $quiz_owner_id = mysqli_query($connection, "SELECT user_id FROM quizzes WHERE quiz_id = $quiz_id limit 1");
@@ -177,6 +188,9 @@ function edit_questions($connection, $user_id, $quiz_id, $content){
     } else {return 0;}
 }
 
+// Edits answers if the user is permited to.
+//The lvl 0 admin can edit everything,
+//while lvl 1 teacher can edit only those things that were created by him and have his user id attached
 function edit_answers($connection, $user_id, $quiz_id, $content, $is_right){
     $user_lvl = mysqli_query($connection, "SELECT level_of_user FROM users WHERE $user_id = user_id limit 1");
     $quiz_owner_id = mysqli_query($connection, "SELECT user_id FROM quizzes WHERE quiz_id = $quiz_id limit 1");
@@ -187,6 +201,7 @@ function edit_answers($connection, $user_id, $quiz_id, $content, $is_right){
     } else {return 0;}
 }
 
+// Fetches questions that are attached to a quiz's id
 function get_questions_by_quiz_id($connection, $quiz_id){
     $query = "SELECT * FROM questions WHERE quiz_id = $quiz_id";
     $results = $connection->query($query);
@@ -194,7 +209,8 @@ function get_questions_by_quiz_id($connection, $quiz_id){
     return $row;
 }
 
-function get_da_answer($connection, $question_id){
+// Fetches answers that are attached to a question's id
+function get_answer_by_question_id($connection, $question_id){
     $query = "SELECT * FROM answers WHERE question_id = $question_id";
     $results = $connection->query($query);
     $row = mysqli_fetch_all($results, MYSQLI_ASSOC);
